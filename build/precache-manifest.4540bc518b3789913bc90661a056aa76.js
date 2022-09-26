ddbf63681cae6666df168c5d50678068595da4ca
BairamovAllaz <ellezbayramov2004@gmail.com>
2022-09-26 14:37:15 +0300

    new version

---
+++ build/precache-manifest.4540bc518b3789913bc90661a056aa76.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4969372cd153a701cbed",
+    "url": "/static/css/main.e0783ef1.chunk.css"
+  },
+  {
+    "revision": "4969372cd153a701cbed",
+    "url": "/static/js/main.4969372c.chunk.js"
+  },
+  {
+    "revision": "2bc649a667ccbd873989",
+    "url": "/static/js/1.2bc649a6.chunk.js"
+  },
+  {
+    "revision": "0d5467721e212ad35ca2",
+    "url": "/static/css/2.4bbd372d.chunk.css"
+  },
+  {
+    "revision": "0d5467721e212ad35ca2",
+    "url": "/static/js/2.0d546772.chunk.js"
+  },
+  {
+    "revision": "ea945247b11b919aeca4",
+    "url": "/static/js/runtime~main.ea945247.js"
+  },
+  {
+    "revision": "d58d7030fe5ab440d753ad85bb8415cd",
+    "url": "/index.html"
+  }
+];
